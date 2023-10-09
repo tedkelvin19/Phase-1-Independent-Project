3242949b78daa9cbb0fca9ca364a239c8a9dec7f
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-10-09 13:02:39 +0300

    added a function to create DOM Elements

---
+++ index.js
@@ -0,0 +1,16 @@
+// DOM Render function
+function renderCar(car){
+    // build car container
+    let card = document.createElement('div')
+    card.className = "car"
+    card.innerHTML = `
+    <img src="${car.image}">
+	<p class="title">${car.title}</p>
+	<p class="year">${car.start_production}</p>
+	<p class="class">
+	    <span>${car.class}</span>
+	</p>
+    `
+    // Add the car to the DOM
+    document.querySelector('.cars').appendChild(card)
+}
